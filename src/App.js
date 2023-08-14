338d5bbbb5a75a06abc8e7ee0ffa416144db5c1a
Reem Salti <reem@Reems-MacBook-Pro.local>
2023-08-14 17:26:50 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import fluxlogo from './fluxlogo.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={fluxlogo} className="App-logo" alt="" />
+        <p className='CS'>
+          COMING SOON
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
